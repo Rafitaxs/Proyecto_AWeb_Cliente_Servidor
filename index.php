8bf5e4e4269f095c041af33c9ec7a77919d15734
Rafael Solano S. <rafaelsolanosilv@gmail.com>
2025-07-02 20:28:32 -0600

    Actualización de archivos

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-<?php
-    echo"Hola, mundo!";
 
+<body>
+    <!--Pagina para el inicio de sesion-->
+    <section>
+        <div>
+            <h1>Esta página es para los inicios de sesión</h1>
+        </div>
+    </section>
+
+    <?php
+
+    echo "Hola, mundo!";
 
     ?>
+
+
 </body>
+
 </html>
