e5a4a45b58d08ae5f7c9ed9c0225348a8a9bdee9
Rafael Solano S. <rafaelsolanosilv@gmail.com>
2025-08-04 17:34:57 -0600

    Add files via upload
    Update de la DB

---
+++ clienteservidor.sql
@@ -0,0 +1,184 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 05, 2025 at 01:34 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `clienteservidor`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inscripcion`
+--
+
+CREATE TABLE `inscripcion` (
+  `ID_Inscripcion` int(11) NOT NULL,
+  `NumeroFila` int(11) NOT NULL,
+  `ID` int(11) NOT NULL,
+  `Cedula` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pago`
+--
+
+CREATE TABLE `pago` (
+  `ID` int(11) NOT NULL,
+  `BIC` varchar(11) NOT NULL,
+  `Monto` int(11) NOT NULL,
+  `Fecha_Pago` date NOT NULL,
+  `Cedula` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sede`
+--
+
+CREATE TABLE `sede` (
+  `ID` int(11) NOT NULL,
+  `Nombre` varchar(30) NOT NULL,
+  `Provincia` varchar(15) NOT NULL,
+  `Canton` varchar(25) NOT NULL,
+  `Distrito` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `Cedula` int(11) NOT NULL,
+  `Nombre` varchar(30) NOT NULL,
+  `Apellido1` varchar(30) NOT NULL,
+  `Apellido2` varchar(30) NOT NULL,
+  `Correo` varchar(30) NOT NULL,
+  `Telefono` int(11) NOT NULL,
+  `Provincia` varchar(10) NOT NULL,
+  `Canton` varchar(19) NOT NULL,
+  `Distrito` varchar(25) NOT NULL,
+  `DireccionExacta` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehiculo`
+--
+
+CREATE TABLE `vehiculo` (
+  `Placa` varchar(6) NOT NULL,
+  `PMA` int(11) NOT NULL,
+  `CC` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vehiculousuario`
+--
+
+CREATE TABLE `vehiculousuario` (
+  `ID_Vehiculo` int(11) NOT NULL,
+  `Cedula` int(11) NOT NULL,
+  `Placa` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `inscripcion`
+--
+ALTER TABLE `inscripcion`
+  ADD PRIMARY KEY (`ID_Inscripcion`),
+  ADD KEY `Id` (`ID`),
+  ADD KEY `Cedula` (`Cedula`);
+
+--
+-- Indexes for table `pago`
+--
+ALTER TABLE `pago`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `Cedula` (`Cedula`);
+
+--
+-- Indexes for table `sede`
+--
+ALTER TABLE `sede`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`Cedula`);
+
+--
+-- Indexes for table `vehiculo`
+--
+ALTER TABLE `vehiculo`
+  ADD PRIMARY KEY (`Placa`);
+
+--
+-- Indexes for table `vehiculousuario`
+--
+ALTER TABLE `vehiculousuario`
+  ADD PRIMARY KEY (`ID_Vehiculo`),
+  ADD KEY `Cedula` (`Cedula`),
+  ADD KEY `Placa` (`Placa`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `inscripcion`
+--
+ALTER TABLE `inscripcion`
+  MODIFY `ID_Inscripcion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pago`
+--
+ALTER TABLE `pago`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sede`
+--
+ALTER TABLE `sede`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `vehiculousuario`
+--
+ALTER TABLE `vehiculousuario`
+  MODIFY `ID_Vehiculo` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
