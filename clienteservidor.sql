adc279bf34bbfd8567c4eb7c28cfb21ad383314f
Rafael Solano S. <rafaelsolanosilv@gmail.com>
2025-08-11 16:54:01 -0600

    Update clienteservidor.sql

--- clienteservidor.sql
+++ clienteservidor.sql
@@ -134,20 +134,20 @@ VALUES ('7', 'Limon');
 
 
 
-INSERT INTO `inscripcion` (`ID_Inscripcion`, `Cedula`, `Nombre`, `Apellido`, `TipoLicencia`) 
-VALUES ('1', '101010101', 'María', 'Gómez', 'B1');
+INSERT INTO `inscripcion` (`ID_Inscripcion`, `Cedula`, `Nombre`, `Apellido`, `TipoLicencia`, `SedeID`) 
+VALUES ('1', '101010101', 'María', 'Gómez', 'B1', `1`);
 
-INSERT INTO `inscripcion` (`ID_Inscripcion`, `Cedula`, `Nombre`, `Apellido`, `TipoLicencia`) 
-VALUES ('2', '202020202', 'Carlos', 'Ramírez', 'C2');
+INSERT INTO `inscripcion` (`ID_Inscripcion`, `Cedula`, `Nombre`, `Apellido`, `TipoLicencia`, `SedeID`) 
+VALUES ('2', '202020202', 'Carlos', 'Ramírez', 'C2', `2`);
 
-INSERT INTO `inscripcion` (`ID_Inscripcion`, `Cedula`, `Nombre`, `Apellido`, `TipoLicencia`) 
-VALUES ('3', '303030303', 'Ana', 'López', 'A1');
+INSERT INTO `inscripcion` (`ID_Inscripcion`, `Cedula`, `Nombre`, `Apellido`, `TipoLicencia`, `SedeID`) 
+VALUES ('3', '303030303', 'Ana', 'López', 'A1', `3`);
 
-INSERT INTO `inscripcion` (`ID_Inscripcion`, `Cedula`, `Nombre`, `Apellido`, `TipoLicencia`) 
-VALUES ('4', '404040404', 'Luis', 'Fernández', 'B2');
+INSERT INTO `inscripcion` (`ID_Inscripcion`, `Cedula`, `Nombre`, `Apellido`, `TipoLicencia`, `SedeID`) 
+VALUES ('4', '404040404', 'Luis', 'Fernández', 'B2',`4`);
 
-INSERT INTO `inscripcion` (`ID_Inscripcion`, `Cedula`, `Nombre`, `Apellido`, `TipoLicencia`) 
-VALUES ('5', '505050505', 'Sofía', 'Martínez', 'C3');
+INSERT INTO `inscripcion` (`ID_Inscripcion`, `Cedula`, `Nombre`, `Apellido`, `TipoLicencia`, `SedeID`) 
+VALUES ('5', '505050505', 'Sofía', 'Martínez', 'C3',`5`);
 
 
 CREATE TABLE cupos_sede (
